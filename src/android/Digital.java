ad9abc485291245de5854b9b186a4f9e6c3489b8
Rodrigo Raúl Canales Zorrilla <rcanalez@everis.com>
2018-12-20 11:34:31 -0500

    capture

--- src/android/Digital.java
+++ src/android/Digital.java
@@ -175,6 +175,20 @@ public class Digital extends CordovaPlugin {
 
             byteArray = data.getByteArrayExtra("bytearray");
             Log.i(TAG,"byteArray: "+byteArray);
+
+
+            String encoded = encode(byteArray);
+
+            if(encoded != null){
+                Log.i(TAG, "got b64 new");
+                Log.i(TAG, encoded);
+
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, encoded);            
+                callbackContext.sendPluginResult(pluginResult);
+            }
+
+            
+
             //bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
 
             
@@ -197,7 +211,7 @@ public class Digital extends CordovaPlugin {
 
 
 
-
+    //CHECK CONNECTION FAILED
     private void displayReaderNotFound()
 	{
 		// empty serial number means reader not found		
@@ -221,7 +235,11 @@ public class Digital extends CordovaPlugin {
 
 		AlertDialog alertDialog = alertDialogBuilder.create();
 		alertDialog.show();		
+
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "Reader not found");
+        callbackContext.sendPluginResult(pluginResult);
 	}
+    //CHECK CONNECTION SUCCESS
     protected void CheckDevice() {
 		try {
 			m_reader.Open(Priority.EXCLUSIVE);
@@ -234,6 +252,10 @@ public class Digital extends CordovaPlugin {
 
 			m_reader.Close();
 			Globals.getInstance().enableCamera();
+
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, "Reader connected");
+            callbackContext.sendPluginResult(pluginResult);
+
 		} catch (UareUException e1) {
 			displayReaderNotFound();
 		}
@@ -258,6 +280,48 @@ public class Digital extends CordovaPlugin {
 	    	}
 	    }
 	};	
+
+
+
+
+
+
+    private String encode(byte[] d) {
+        if (d == null) {
+            return null;
+        }
+        int idx;
+        byte[] data = new byte[(d.length + 2)];
+        System.arraycopy(d, 0, data, 0, d.length);
+        byte[] dest = new byte[((data.length / 3) * 4)];
+        int sidx = 0;
+        int didx = 0;
+        while (sidx < d.length) {
+            dest[didx] = (byte) ((data[sidx] >>> 2) & 63);
+            dest[didx + 1] = (byte) (((data[sidx + 1] >>> 4) & 15) | ((data[sidx] << 4) & 63));
+            dest[didx + 2] = (byte) (((data[sidx + 2] >>> 6) & 3) | ((data[sidx + 1] << 2) & 63));
+            dest[didx + 3] = (byte) (data[sidx + 2] & 63);
+            sidx += 3;
+            didx += 4;
+        }
+        for (idx = 0; idx < dest.length; idx++) {
+            if (dest[idx] < (byte) 26) {
+                dest[idx] = (byte) (dest[idx] + 65);
+            } else if (dest[idx] < (byte) 52) {
+                dest[idx] = (byte) ((dest[idx] + 97) - 26);
+            } else if (dest[idx] < (byte) 62) {
+                dest[idx] = (byte) ((dest[idx] + 48) - 52);
+            } else if (dest[idx] < (byte) 63) {
+                dest[idx] = (byte) 43;
+            } else {
+                dest[idx] = (byte) 47;
+            }
+        }
+        for (idx = dest.length - 1; idx > (d.length * 4) / 3; idx--) {
+            dest[idx] = (byte) 61;
+        }
+        return new String(dest);
+    }
 
 
 
